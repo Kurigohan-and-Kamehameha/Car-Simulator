016b2488b1fe8e3bce1b1ac05c4fb12e77aa2d89
Kurigohan-and-Kamehameha <zarnkemarlon@yahoo.de>
2026-04-01 23:57:49 +0200

    removed unused classes

--- backend/src/main/java/org/example/cargame/persistence/PathSaveData.java
+++
@@ -1,9 +0,0 @@
-package org.example.cargame.persistence;
-
-import java.util.List;
-
-public class PathSaveData {
-    public List<String> fromNodes;
-    public List<String> toNodes;
-    public int currentEdgeIndex;
-}
--- backend/src/main/java/org/example/cargame/persistence/SaveGame.java
+++
@@ -1,7 +0,0 @@
-package org.example.cargame.persistence;
-
-import java.util.Map;
-
-public class SaveGame {
-    public Map<Integer, EntityData> entities;
-}
